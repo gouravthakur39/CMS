d3466c27be4a7dbd5518b3fbeecf01b87ff02f76
gouravthakur39 <gourav.thakur39@gmail.com>
2020-02-18 18:30:33 +0530

    configure express server, connect mongodb

--- app.js
+++ app.js
@@ -1,3 +1,38 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const path = require('path')
+
+const app = express();
+
+// configure mongoose to connect to mongoDB
+mongoose.connect('mongodb://localhost:27017/cms', 
+    {useNewUrlParser:true})
+    .then(response => {
+        console.log('MongoDB successfully connected');
+    }).catch(err => {
+        console.log('Database connection failed');
+    });
+
+// configuring express
+app.use(express
+    .json()
+);
+
+app.use(express.urlencoded({
+    extended:true }
+));
+
+app.use(express.static(path
+    .join(__dirname, 'public')
+));
+
+// Routes
+app.use('/', (req, res) => {
+    res.send('Welcome to CMS');
+});
+
+
+app.listen(3000, () => {
+    console.log('Server running on port 3000');
+});
 
