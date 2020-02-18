25714a1c717ad2827cdefd2632db3e18104f49a1
gouravthakur39 <gourav.thakur39@gmail.com>
2020-02-18 20:34:07 +0530

    segregated files to partials

--- app.js
+++ app.js
@@ -1,46 +1,48 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const path = require('path')
-const hbs = require('express-handlebars')
-const {mongodbUrl} = require('./config/configuration')
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+const hbs = require("express-handlebars");
+const { mongodbUrl, PORT } = require("./config/configuration");
+
 
 const app = express();
 
 // configure mongoose to connect to mongoDB
-mongoose.connect(mongodbUrl, 
-    {useNewUrlParser:true})
-    .then(response => {
-        console.log('MongoDB successfully connected');
-    }).catch(err => {
-        console.log('Database connection failed');
-    });
+mongoose
+  .connect(mongodbUrl, { useNewUrlParser: true })
+  .then(response => {
+    console.log("MongoDB successfully connected");
+  })
+  .catch(err => {
+    console.log("Database connection failed");
+  });
 
 // configuring express
-app.use(express
-    .json()
+app.use(express.json());
+
+app.use(
+  express.urlencoded({
+    extended: true
+  })
 );
 
-app.use(express.urlencoded({
-    extended:true }
-));
-
-app.use(express.static(path
-    .join(__dirname, 'public')
-));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Setup view engine with Handlebars
-app.engine('handlebars', hbs({
-    defaultLayout: 'default'
-}));
-app.set('view engine', 'handlebars');
+app.engine(
+  "handlebars",
+  hbs({
+    defaultLayout: "default"
+  })
+);
+app.set("view engine", "handlebars");
 
 // Routes
-app.use('/', (req, res) => {
-    res.render('default/index');
+app.use("/", (req, res) => {
+  res.render("default/index");
 });
 
-
-app.listen(3000, () => {
-    console.log('Server running on port 3000');
+/* Start The Server */
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
-
--- config/configuration.js
+++ config/configuration.js
@@ -1,3 +1,4 @@
 module.exports = {
-   mongodbUrl: 'mongodb://localhost:27017/cms'
+   mongodbUrl: 'mongodb://localhost:27017/cms',
+   PORT: process.env.PORT || 3000,
 }
